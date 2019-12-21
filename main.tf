ee604440172a964a381097bb36c6d85bf665ccc5
wk-j <somnuk.wk@gmail.com>
2019-12-22 03:53:07 +0700

    Ssh key

--- main.tf
+++ main.tf
@@ -1,15 +1,21 @@
 variable "do_token" {}
 
+resource "digitalocean_ssh_key" "default" {
+  name       = "Terraform Example"
+  public_key = file("/Users/wk/.ssh/id_rsa.pub")
+}
+
 provider "digitalocean" {
   token = "${var.do_token}"
 }
 
 resource "digitalocean_droplet" "terraform" {
-  image  = "docker-18-04"
-  name   = "Terraform-101"
-  region = "sgp1"
-  size   = "s-1vcpu-2gb"
-  tags   = ["Test", "Terraform"]
+  image    = "docker-18-04"
+  name     = "terraform-101"
+  region   = "sgp1"
+  size     = "s-1vcpu-2gb"
+  tags     = ["terraform"]
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 }
 
 output "droplet_output" {
