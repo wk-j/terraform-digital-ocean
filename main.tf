200980bb61c3e4119fb6987df318d7b602586eff
wk-j <somnuk.wk@gmail.com>
2019-12-22 03:40:11 +0700

    Output

--- main.tf
+++ main.tf
@@ -4,9 +4,14 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-resource "digitalocean_droplet" "web" {
+resource "digitalocean_droplet" "terraform" {
   image  = "docker-18-04"
-  name   = "Web101"
+  name   = "Terraform-101"
   region = "sgp1"
   size   = "s-1vcpu-2gb"
+  tags   = ["Test", "Terraform"]
+}
+
+output "droplet_output" {
+  value = digitalocean_droplet.terraform.ipv4_address
 }
